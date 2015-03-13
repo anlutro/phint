1ce1a7892ae0262213140930c9792ce1eb924c04
Andreas Lutro <anlutro@gmail.com>
2015-03-13 22:30:55 +0100

    fix tests

--- tests/functional/InstanceofTest.php
+++ tests/functional/InstanceofTest.php
@@ -7,6 +7,6 @@ class InstanceofTest extends FunctionalTestCase
 		$errors = $this->check(__DIR__.'/cases/InstanceofCase.php');
 		$this->assertEquals(1, count($errors));
 		$this->assertEquals('6', $errors[0]->getLineNumber());
-		$this->assertEquals('Testing instanceof against non-existant class: \NonexistantClass', $errors[0]->getMessage());
+		$this->assertEquals('Testing instanceof against non-existant class: NonexistantClass', $errors[0]->getMessage());
 	}
 }
--- tests/functional/NewTest.php
+++ tests/functional/NewTest.php
@@ -7,6 +7,6 @@ class NewTest extends FunctionalTestCase
 		$errors = $this->check(__DIR__.'/cases/NewCase.php');
 		$this->assertEquals(1, count($errors));
 		$this->assertEquals('8', $errors[0]->getLineNumber());
-		$this->assertEquals('Tried instantiating non-existant class: \NonExistant', $errors[0]->getMessage());
+		$this->assertEquals('Tried instantiating non-existant class: NonExistant', $errors[0]->getMessage());
 	}
 }
