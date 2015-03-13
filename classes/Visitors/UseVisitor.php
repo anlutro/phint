46dd07f1239255fc0099f9215bc5298bbf6aa228
Andreas Lutro <anlutro@gmail.com>
2015-03-13 22:31:15 +0100

    add error when importing non-existant class

--- classes/Visitors/UseVisitor.php
+++ classes/Visitors/UseVisitor.php
@@ -2,6 +2,7 @@
 namespace Phint\Visitors;
 
 use Phint\AbstractNodeVisitor;
+use Phint\Error;
 use Phint\NodeVisitorInterface;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Use_;
@@ -17,7 +18,30 @@ class UseVisitor extends AbstractNodeVisitor implements NodeVisitorInterface
 		$ctx = $this->getContext();
 
 		foreach ($node->uses as $use) {
+			$className = $use->name->toString();
+			if (!$this->classExists($className)) {
+				$this->addError($this->createClassNotFoundError($className, $node));
+			}
 			$ctx->import($use->name->toString(), $use->alias);
 		}
+	}
+
+	private function classExists($className)
+	{
+		if (PHP_VERSION_ID > 54000) {
+			return class_exists($className)
+				|| interface_exists($className)
+				|| trait_exists($className);
+		} else {
+			return class_exists($className)
+				|| interface_exists($className);
+		}
+	}
+
+	private function createClassNotFoundError($className, Use_ $node)
+	{
+		$className = ltrim($className, '\\');
+		$msg = "Importing non-existant class: $className";
+		return new Error($msg, $node);
 	}
 }
