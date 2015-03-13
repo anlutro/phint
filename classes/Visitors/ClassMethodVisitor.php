5ad4fb350125ed4a721369f78e000c98417c1ddb
Andreas Lutro <anlutro@gmail.com>
2015-03-13 22:44:48 +0100

    add error on type-hinting non-existant class

--- classes/Visitors/ClassMethodVisitor.php
+++ classes/Visitors/ClassMethodVisitor.php
@@ -2,8 +2,11 @@
 namespace Phint\Visitors;
 
 use Phint\AbstractNodeVisitor;
+use Phint\Error;
 use Phint\NodeVisitorInterface;
 use PhpParser\Node;
+use PhpParser\Node\Name;
+use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\ClassMethod;
 
 class ClassMethodVisitor extends AbstractNodeVisitor implements NodeVisitorInterface
@@ -22,9 +25,32 @@ class ClassMethodVisitor extends AbstractNodeVisitor implements NodeVisitorInter
 		$ctx->setReflectionFunction($reflClass->getMethod($node->name));
 
 		foreach ($node->params as $param) {
+			if ($param->type instanceof Name) {
+				$className = $ctx->getClassName($param->type->toString());
+				if (!$this->classExists($className)) {
+					$this->addError($this->createClassNotFoundError(
+						$className, $node, $param
+					));
+				}
+			}
 			$ctx->setVariable($param->name, $param);
 		}
 
 		$this->recurse($node->stmts);
+	}
+
+	private function classExists($className)
+	{
+		return class_exists($className) || interface_exists($className);
+	}
+
+	private function createClassNotFoundError($className, ClassMethod $node,
+		Param $param)
+	{
+		$className = ltrim($className, '\\');
+		$method = $node->name.'::'.$param->name;
+		$msg = "$method() argument \${$param->name} is type-hinted "
+			. "against a non-existant class: $className";
+		return new Error($msg, $param);
 	}
 }
