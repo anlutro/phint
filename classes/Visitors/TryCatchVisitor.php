86603faa018f9cb5218f8d7a1d51e82e523a24c8
Andreas Lutro <anlutro@gmail.com>
2015-03-14 08:20:45 +0100

    add error on catching non-exception class

--- classes/Visitors/TryCatchVisitor.php
+++ classes/Visitors/TryCatchVisitor.php
@@ -4,6 +4,7 @@ namespace Phint\Visitors;
 use Phint\AbstractNodeVisitor;
 use Phint\Error;
 use Phint\NodeVisitorInterface;
+use ReflectionClass;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\TryCatch;
 use PhpParser\Node\Stmt\Catch_;
@@ -27,7 +28,18 @@ class TryCatchVisitor extends AbstractNodeVisitor implements NodeVisitorInterfac
 				if (!$this->classExists($className)) {
 					$this->addError($this->createClassNotFoundError(
 						$className, $catch->type));
+				} else {
+					$refl = new ReflectionClass($className);
+					static $exception;
+					if (!isset($exception)) {
+						$exception = new \Exception;
+					}
+					if (!$refl->isInstance($exception)) {
+						$this->addError($this->createClassNotExceptionError(
+							$className, $catch->type));
+					}
 				}
+
 			}
 
 			$this->recurse($catch->stmts);
@@ -47,6 +59,13 @@ class TryCatchVisitor extends AbstractNodeVisitor implements NodeVisitorInterfac
 	{
 		$className = ltrim($className, '\\');
 		$msg = "Trying to catch non-existant class: $className";
+		return new Error($msg, $node);
+	}
+
+	private function createClassNotExceptionError($className, Node $node)
+	{
+		$className = ltrim($className, '\\');
+		$msg = "Trying to catch non-exception class: $className";
 		return new Error($msg, $node);
 	}
 }
