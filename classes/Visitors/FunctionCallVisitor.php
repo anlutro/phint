eaff498ff50b689b09fde99304330bd40dd91b68
Andreas Lutro <anlutro@gmail.com>
2015-03-14 08:58:07 +0100

    detect variables assigned by reference via function call
    closes #2

--- classes/Visitors/FunctionCallVisitor.php
+++ classes/Visitors/FunctionCallVisitor.php
@@ -6,7 +6,10 @@ use Phint\Error;
 use Phint\NodeVisitorInterface;
 use PhpParser\Node;
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Name;
+use ReflectionFunction;
+use ReflectionParameter;
 
 class FunctionCallVisitor extends AbstractNodeVisitor implements NodeVisitorInterface
 {
@@ -16,19 +19,46 @@ class FunctionCallVisitor extends AbstractNodeVisitor implements NodeVisitorInte
 			return;
 		}
 
+		// $node->name can also be an expression, in which case the function
+		// name is dynamic and we can't check anything
 		if ($node->name instanceof Name) {
-			$func = $node->name->toString();
-			$fqfn = $this->getContext()
-				->getClassName($func);
-			if (!function_exists($func) && !function_exists($fqfn)) {
-				$this->addError($this->createUndefinedFunctionError($func, $node));
-			}
+			$this->checkFunction($node->name, $node);
 		}
 
 		$argValues = array_map(function($arg) {
 			return $arg->value;
 		}, $node->args);
 		$this->recurse($argValues);
+	}
+
+	private function checkFunction(Name $name, FuncCall $node)
+	{
+		$func = $this->getContext()
+			->getClassName($name->toString());
+
+		if (!function_exists($func)) {
+			$func = $name->toString();
+		}
+
+		if (!function_exists($func)) {
+			$this->addError($this->createUndefinedFunctionError($func, $node));
+			return;
+		}
+
+		// look for function parameters passed by reference
+		$refl = new ReflectionFunction($func);
+		foreach ($refl->getParameters() as $param) {
+			if ($param->isPassedByReference()) {
+				$pos = $param->getPosition();
+				if (isset($node->args[$pos])) {
+					$var = $node->args[$pos]->value;
+					if ($var instanceof Variable) {
+						$this->getContext()
+							->setVariable($var->name, $var);
+					}
+				}
+			}
+		}
 	}
 
 	private function createUndefinedFunctionError($func, FuncCall $node)
