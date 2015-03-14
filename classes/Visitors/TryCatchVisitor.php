4193ea878ed6ce4408782a3a1f3ee4a37301d337
Andreas Lutro <anlutro@gmail.com>
2015-03-14 08:16:33 +0100

    check that caught exceptions exist

--- classes/Visitors/TryCatchVisitor.php
+++ classes/Visitors/TryCatchVisitor.php
@@ -2,9 +2,11 @@
 namespace Phint\Visitors;
 
 use Phint\AbstractNodeVisitor;
+use Phint\Error;
 use Phint\NodeVisitorInterface;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\TryCatch;
+use PhpParser\Node\Stmt\Catch_;
 
 class TryCatchVisitor extends AbstractNodeVisitor implements NodeVisitorInterface
 {
@@ -14,10 +16,37 @@ class TryCatchVisitor extends AbstractNodeVisitor implements NodeVisitorInterfac
 			return;
 		}
 
-		$this->recurse($node->stmts, $node->catches);
+		$this->recurse($node->stmts);
+
+		foreach ($node->catches as $catch) {
+			if ($catch->type) {
+				$className = $catch->type->toString();
+				if ($catch->type->isFullyQualified()) {
+					$className = '\\'.$className;
+				}
+				if (!$this->classExists($className)) {
+					$this->addError($this->createClassNotFoundError(
+						$className, $catch->type));
+				}
+			}
+
+			$this->recurse($catch->stmts);
+		}
 
 		if ($node->finallyStmts) {
 			$this->recurse($node->finallyStmts);
 		}
+	}
+
+	private function classExists($className)
+	{
+		return class_exists($className) || interface_exists($className);
+	}
+
+	private function createClassNotFoundError($className, Node $node)
+	{
+		$className = ltrim($className, '\\');
+		$msg = "Trying to catch non-existant class: $className";
+		return new Error($msg, $node);
 	}
 }
